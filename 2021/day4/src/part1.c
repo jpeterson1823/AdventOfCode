4c20ce50823b7a8de5fb0e697732b79be6cc7156
jpeterson1823 <petersonjohn418@gmail.com>
2022-01-07 11:35:00 -0600

    Almost finished day4 part1. Just need to sum all the unmarked numbers on the board and print it out to get my answer.

--- 2021/day4/src/part1.c
+++ 2021/day4/src/part1.c
@@ -14,7 +14,6 @@ int* get_draws(char* input) {
 	for (int i = 0; i < len; i++)
 		draws_str[i] = input[i];
 	draws_str[len] = '\0';
-	printf("%s\n", draws_str);
 
 	// convert to int* for easy of use and sanity preservation
 	int* draws = malloc(sizeof(int));
@@ -43,9 +42,115 @@ int* get_draws(char* input) {
 	return draws;
 }
 
+
+int** get_board(char* input, int num) {
+	int row_len = 15; 	// 5 2-char numbers, 4 spaces, and one return char: 5*2 + 4 + 1 = 15
+	int num_rows = 5;
+	int num_cols = 5;
+	int npr = 5; 		// numbers per row
+	int step = 3;
+
+	// skip first and second line of input file, as well as other boards according to "num" param
+	char* boards = &input[291 + (row_len*num_rows + 1)*num];
+
+	int** board = malloc(sizeof(int*) * 5);
+	int bi = 0;
+	for (int row = 0; row < num_rows; row++) {
+		int* r = malloc(sizeof(int) * npr);
+		int ri = 0;
+		for (int i = 0; i < row_len; i+=step) {
+			// get number in row as cstring
+			cstring* temp = cString.create(&boards[(row_len*row + i)], step-1);
+			// remove extra spaces
+			cstring* num = cString.strip(temp);
+			cString.destroy(temp);
+			// convert to int and store in r
+			r[ri++] = cString.toi(num);
+			cString.destroy(num);
+		}
+		board[bi++] = r;
+	}
+
+	return board;
+}
+
+void free_board(int** board) {
+	for (int i = 0; i < 5; i++)
+		free(board[i]);
+	free(board);
+}
+
+unsigned long hash(int i) {
+	return i<<1 / 2;
+}
+
+int check_win(int** board, int* draws) {
+	printf("Current Board:\n");
+	for (int r = 0; r < 5; r++) {
+		printf("\t");
+		for (int c = 0; c < 5; c++)
+			printf("%i ", board[r][c]);
+		printf("\n");
+	}
+
+	int step = 5;
+	for (int draw = 0; draw < 95; draw+=step) {
+		// calculate hash
+		unsigned long draw_hash = 0;
+		for (int i = draw; i < draw+step; i++)
+			draw_hash = hash(draws[i]);
+
+		// check each row to see if it's hash matches
+		for (int row = 0; row < 5; row++) {
+			unsigned long row_hash = 0;
+			for (int col = 0; col < 5; col++)
+				row_hash += hash(board[row][col]);
+			if (row_hash == draw_hash)
+				return draw+step;
+		}
+
+		for (int col = 0; col < 5; col++) {
+			unsigned long col_hash = 0;
+			for (int row = 0; row < 5; row++)
+				col_hash = hash(board[col][row]);
+			if (col_hash == draw_hash)
+				return draw+step;
+		}
+
+	}
+
+	return -1;
+}
+
+int calc_unmarked_sum(int** board, int winning_draw) {
+	// TODO
+	return NULL;
+}
+
 void part1(char* input) {
 	// get rolls from first line of file
 	int* draws = get_draws(input);
+
+
+	int best_board = -1;
+	int min_moves = 10000;
+	int** board;
+	for (int i = 0; i < 100; i++) {
+		board = get_board(input, i);
+		int win = check_win(board, draws);
+		free_board(board);
+
+		if (win >= 0) {
+			printf("Board %i is a winner, taking %i draws to win.\n", i, win);
+			if (win < min_moves) {
+				best_board = i;
+				min_moves = win;
+			}
+		}
+	}
+
+	printf("Best Board: %i\n", best_board);
+	board = get_board(input, best_board);
 
 	free(draws);
 }
